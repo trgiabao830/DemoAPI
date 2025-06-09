b151f116315e75db4ec25edc09b52f2e40428b48
trgiabao830 <trgiabao830@gmail.com>
2025-06-09 14:29:47 +0700

    sửa format orderDate

--- src/main/java/com/example/demo/dto/OrderDTO.java
+++ src/main/java/com/example/demo/dto/OrderDTO.java
@@ -3,6 +3,8 @@ package com.example.demo.dto;
 import java.sql.Timestamp;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +12,9 @@ import lombok.Setter;
 @Setter
 public class OrderDTO {
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Ho_Chi_Minh")
     private Timestamp orderDate;
+
     private String status;
     private Long totalAmount;
     private Long userId;
